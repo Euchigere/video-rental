9572a51343baa7735bceddda10d4e57979f5b582
euchigere <ugochigere02gmail.com>
2021-10-07 23:52:26 +0100

    migrate field type

--- src/main/java/com/emmanuelc/videorental/dataloader/BoostrapData.java
+++ src/main/java/com/emmanuelc/videorental/dataloader/BoostrapData.java
@@ -25,53 +25,53 @@ public class BoostrapData implements ApplicationListener<ApplicationReadyEvent> 
         RegularVideo video1 = (RegularVideo) new RegularVideo()
                 .setTitle("Chucky")
                 .setType(VideoType.REGULAR)
-                .setGenre(List.of(Genre.HORROR));
+                .setGenre(Genre.ROMANCE);
 
         RegularVideo video2 = (RegularVideo) new RegularVideo()
                         .setTitle("Walking dead")
                         .setType(VideoType.REGULAR)
-                        .setGenre(List.of(Genre.HORROR, Genre.ACTION));
+                        .setGenre(Genre.HORROR);
 
         RegularVideo video3 = (RegularVideo) new RegularVideo()
                         .setTitle("Equalizer")
                         .setType(VideoType.REGULAR)
-                        .setGenre(List.of(Genre.ACTION));
+                        .setGenre(Genre.COMEDY);
 
         ChildrenMovieVideo video4 = (ChildrenMovieVideo) new ChildrenMovieVideo()
                 .setMaximumAge(10)
                 .setTitle("Moana")
                 .setType(VideoType.CHILDREN_MOVIE)
-                .setGenre(List.of(Genre.DRAMA, Genre.COMEDY));
+                .setGenre(Genre.DRAMA);
 
         ChildrenMovieVideo video5 = (ChildrenMovieVideo) new ChildrenMovieVideo()
                 .setMaximumAge(12)
                 .setTitle("Mulan")
                 .setType(VideoType.CHILDREN_MOVIE)
-                .setGenre(List.of(Genre.ACTION));
+                .setGenre(Genre.ACTION);
 
         ChildrenMovieVideo video6 = (ChildrenMovieVideo) new ChildrenMovieVideo()
                 .setMaximumAge(8)
                 .setTitle("Luca")
                 .setType(VideoType.CHILDREN_MOVIE)
-                .setGenre(List.of(Genre.DRAMA));
+                .setGenre(Genre.DRAMA);
 
         NewReleaseVideo video7 = (NewReleaseVideo) new NewReleaseVideo()
                 .setYearReleased("2020")
                 .setTitle("Spectre")
                 .setType(VideoType.NEW_RELEASE)
-                .setGenre(List.of(Genre.ACTION, Genre.HORROR));
+                .setGenre(Genre.ACTION);
 
         NewReleaseVideo video8 = (NewReleaseVideo) new NewReleaseVideo()
                 .setYearReleased("2021")
                 .setTitle("Fast and Furious")
                 .setType(VideoType.NEW_RELEASE)
-                .setGenre(List.of(Genre.ACTION));
+                .setGenre(Genre.ACTION);
 
         NewReleaseVideo video9 = (NewReleaseVideo) new NewReleaseVideo()
                 .setYearReleased("2019")
                 .setTitle("Skylines")
                 .setType(VideoType.NEW_RELEASE)
-                .setGenre(List.of(Genre.DRAMA, Genre.COMEDY));
+                .setGenre(Genre.DRAMA);
 
         videoRepo.saveAll(List.of(video1, video2, video3, video4, video5, video6, video7, video8, video9));
     }
--- src/main/java/com/emmanuelc/videorental/domain/models/Video.java
+++ src/main/java/com/emmanuelc/videorental/domain/models/Video.java
@@ -2,14 +2,12 @@ package com.emmanuelc.videorental.domain.models;
 
 import com.emmanuelc.videorental.domain.models.enumerations.Genre;
 import com.emmanuelc.videorental.domain.models.enumerations.VideoType;
-import com.emmanuelc.videorental.utils.GenreListConverter;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Getter
 @Setter
@@ -27,6 +25,6 @@ public abstract class Video extends BaseModel {
     @NotNull protected VideoType type;
 
     @Column(nullable = false)
-    @Convert(converter = GenreListConverter.class)
-    @NotNull private List<Genre> genre;
+    @Enumerated(EnumType.STRING)
+    @NotNull private Genre genre;
 }
